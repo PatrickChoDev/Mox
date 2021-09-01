0ba374a04987ec8a7f2bcf536acac7b4070bded9
Thanapat Chotipun <devpatrick.cho@gmail.com>
2021-09-01 09:26:04 +0700

    [U] Button style

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { CSSProperties, HTMLAttributes } from "react"
 
-export class Button extends React.Component {
-  constructor(props: {} | Readonly<{}>) {
+export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  variant? : "primary" | "secondary" | "ghost" | "link" | "text"
+  colors?: string | string[]
+  theme?: 'dark' | 'light'
+}
+export class Button extends React.Component<ButtonProps,{},{}> {
+  styles: CSSProperties
+  constructor(props: ButtonProps | Readonly<ButtonProps>) {
     super(props)
+    
+    this.props.colors?this.styles.color = this.props.colors[this.props.theme==='dark'?1:0]:null
   }
 
+
   render() {
-    return <button className="btn" {...this.props}>{this.props.children}</button>
+    return (
+      <button className={['btn',this.props.variant].join(' ')} style={this.styles} {...this.props}>
+        {this.props.children}
+      </button>
+    )
   }
 }
